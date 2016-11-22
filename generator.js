79f3a4bbcd7d3ce1b7d233e3da3970ee8d61d809
Dmitry Khalansky <rouanth@gmail.com>
2016-11-22 17:19:46 +0300

    Implement better field generator
    This one presumably can generate all the possible layouts depending on
    the seed instead of a small particular set of them.

--- generator.js
+++ generator.js
@@ -1,15 +1,22 @@
 function generate (width, height, mines, seed) {
-	if (mines > width * height || width < 0 || height < 0 || !seed ||
-			seed >= (1 <<< 31) - 1) {
+	if (mines > elems || width <= 0 || height <= 0) {
 		return null;
 	}
+	var elems = width * height;
 
-	var prime = (1 <<< 31) - 1;
-	var field = create_matrix(height, width, function() { return 0; });
+	var coeffs_arr = [elems - mines];
+	for (var i = 1; i <= mines; ++i) {
+		coeffs_arr.push(coeffs_arr[i-1] * (elems - mines + i));
+	}
 
-	for (var i = 0; i < mines; ++i) {
-		var idx = (i + seed) * prime % width * height;
+	var field = create_matrix(height, width, function() { return false; });
+
+	var rest = create_map(elems, function(i) { return i; });
+	for (var i = mines - 1; i >= 0; --i) {
+		var r_idx = Math.floor(seed / coeffs_arr[i]) % rest.length;
+		var idx = rest.splice(r_idx, 1)[0];
 		field[Math.floor(idx / width)][idx % width] = true;
+		seed %= coeffs_arr[i];
 	}
 
 	return field;
